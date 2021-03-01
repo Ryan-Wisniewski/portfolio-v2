d2e9c88a73e2ef85cfb8d89932eb48f3b515af3d
Ryan-Wisniewski <Ryanwis98@yahoo.com>
2021-03-01 17:16:29 -0500

    reworked projects section included new work and helped the old text content

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 const Welcome = () => {
 
-
     return (
         <div id='home' className='welcome'>
             <a href='#about'><h1 className='onScroll'>Welcome to the Madhouse</h1></a>
